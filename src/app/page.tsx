399fdecf12bf51a3d6665ed6c77d9645ae474d4e
lenny hu <a97251292@gmail.com>
2026-02-01 08:38:37 +0000

    现在在移动端打开的时候，下方的按钮一般都在第一屏看不到，这个需要优化一下

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { LanguageSwitcher } from '@/components/language-switcher';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 md:p-6">
+    <main className="flex min-h-screen w-full flex-col items-center justify-start md:justify-center bg-background p-4 md:p-6">
       <PasswordGenerator />
       <LanguageSwitcher />
     </main>
