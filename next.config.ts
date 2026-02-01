5c5488ed09749e29263682b67a8ad5048e91c785
lenny <imhule@qq.com>
2026-02-02 00:02:10 +0800

    调整pages配置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/pass-genuis',
+  assetPrefix: '/pass-genuis/',
+  trailingSlash: false,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
---
+++ post-build.js
@@ -0,0 +1,10 @@
+// for github pages
+const fs = require('fs');
+const path = require('path');
+
+// 定义 .nojekyll 文件路径（out 根目录）
+const noJekyllPath = path.join(__dirname, './out/.nojekyll');
+
+// 新建空的 .nojekyll 文件（无则创建，有则覆盖为空）
+fs.writeFileSync(noJekyllPath, 'hello', 'utf-8');
+console.log('✅ 已在 out 目录自动生成 .nojekyll 文件');
